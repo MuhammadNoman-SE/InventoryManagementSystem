14e59a9d318d83f00f998c1760530ede71b85db1
muhammad.noman <muhammad.noman@bentley.com>
2021-02-04 17:55:40 +0500

    Added Service

---
+++ InventoryManagementSystem/InventoryManagementSystem/IInventoryManagementSystemService.cs
@@ -0,0 +1,22 @@
+﻿using InventoryManagementSystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem
+{
+
+    [ServiceContract]
+    public interface IInventoryManagementSystemService
+    {
+        [OperationContract]
+        List<Product> GetProducts();
+        [OperationContract()]
+        List<Customer> GetCustomers();
+        [OperationContract]
+        void SubmitOrder(Order order);
+    }
+}
---
+++ InventoryManagementSystem/InventoryManagementSystem/InventoryManagementSystemService.cs
@@ -0,0 +1,40 @@
+﻿using InventoryManagementSystem.Data;
+using InventoryManagementSystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem
+{
+    [ServiceBehavior(InstanceContextMode =InstanceContextMode.PerCall)]
+    public class InventoryManagementSystemService : IInventoryManagementSystemService, IDisposable
+    {
+        InventoryManagementSystemDbContext _Context = new InventoryManagementSystemDbContext();
+        public void Dispose()
+        {
+            _Context.Dispose();
+        }
+
+        public List<Product> GetProducts()
+        {
+            return _Context.Products.ToList();
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            return _Context.Customers.ToList();
+        }
+
+        [OperationBehavior(TransactionScopeRequired =true)]
+        public void SubmitOrder(Order order)
+        {
+            _Context.Orders.Add(order);
+            order.OrderItems.ForEach(oi => _Context.OrderItems.Add(oi));
+            _Context.SaveChanges();
+        }
+    }
+}
