0ef485ace8bd29f471240c67106f720c9749dae9
muhammad.noman <muhammad.noman@bentley.com>
2021-02-06 10:11:59 +0500

    hOSTED sERVICE ON WEB

---
+++ InventoryManagementSystem/InventoryManagementSystemWebApplication/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace InventoryManagementSystemWebApplication.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ InventoryManagementSystem/InventoryManagementSystemWebApplication/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InventoryManagementSystemWebApplication.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
