045fa8d070ab74574791df296fe94398584c827a
muhammad.noman <muhammad.noman@bentley.com>
2021-02-06 09:53:47 +0500

    Self hosted service

---
+++ InventoryManagementSystem/InventoryManagementSystemConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using InventoryManagementSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystemConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(InventoryManagementSystemService));
+            host.Open();
+            Console.ReadLine();
+            host.Close();
+        }
+    }
+}
